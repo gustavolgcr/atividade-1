987b094e72b677e08a93ff8e9d117bafb587b686
Gustavo Coutinho <gustavolgcr@gmail.com>
2019-10-03 16:59:47 -0300

    Iniciando o código da calculadora

---
+++ calculadora.py
@@ -0,0 +1,4 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+operacao = int(input("Digite a operação que deseja realizar: (1) Soma, (2) Subtração, (3) Multiplicação ou (4) Divisão.\n"))
