64c18b08a15695671fcfb27a7c90e866230c19de
Gustavo Coutinho <gustavolgcr@gmail.com>
2019-10-03 18:43:16 -0300

    Adicionando mais uma possibilidade no menu

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
-operacao = int(input("Digite a operação que deseja realizar: (1) Soma, (2) Subtração, (3) Multiplicação ou (4) Divisão.\n"))
+operacao = int(input("Digite a operação que deseja realizar: (1) Soma, (2) Subtração, (3) Multiplicação, (4) Divisão ou (5) Exponenciação.\n"))
